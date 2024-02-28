faa9842bd614599a89175c75e73f49362f456ac7
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-02-28 22:48:10 +0800

    manual ci🤓

--- dist/kikey.js
+++ dist/kikey.js
@@ -1,4 +1,4 @@
-const p = /* @__PURE__ */ new Set([
+const v = /* @__PURE__ */ new Set([
   "space",
   "dash",
   "arrowleft",
@@ -25,81 +25,45 @@ const p = /* @__PURE__ */ new Set([
   "f9",
   "f10",
   "f11",
-  "f12",
+  "f12"
 ]);
-function k(l) {
-  const t = {
-    ctrl: !1,
-    shift: !1,
-    alt: !1,
-    // Option key on Macintosh
-    meta: !1,
-    // Command key on Macintosh
-    key: l[l.length - 1],
-  };
-  for (const s of l.split("-")) {
-    s.length == 1
-      ? (t.ctrl = s == "C" || t.ctrl,
-        t.shift = s == "S" || t.shift,
-        t.alt = s == "A" || t.alt,
-        t.meta = s == "M" || t.meta)
-      : p.has(s) && (t.key = s === "space" ? " " : s === "dash" ? "-" : s);
-  }
-  if (t.key === void 0 || l[l.length - 1] === "-") {
-    throw Error("Invalid binding sequence.");
-  }
-  return l.length == 1 &&
-    (t.ctrl
-      ? t.key = "control"
-      : t.shift
-      ? t.key = "shift"
-      : t.alt
-      ? t.key = "alt"
-      : t.meta && (t.key = "meta")),
-    t;
+function L(n) {
+  let r = !1, a = !1, c = !1, l = !1, s;
+  for (const t of n.split("-"))
+    if (t.length == 1)
+      r = t == "C" || r, a = t == "S" || a, c = t == "A" || c, l = t == "M" || l, s = t;
+    else if (v.has(t))
+      s = t == "space" ? " " : t == "dash" ? "-" : t;
+    else
+      throw Error("Invalid binding.");
+  return n.length == 1 && (r ? s = "control" : a ? s = "shift" : c ? s = "alt" : l && (s = "meta")), { ctrlKey: r, shiftKey: a, altKey: c, metaKey: l, key: s };
 }
-function u(l) {
-  if (!document) {
+function C(n) {
+  if (!document)
     throw Error("Only support browser environment.");
+  n || (n = document);
+  const r = /* @__PURE__ */ new Map();
+  let a, c, l = !0;
+  function s(e) {
+    return ["control", "shift", "alt", "meta"].includes(e.toLowerCase());
   }
-  l || (l = document);
-  const t = /* @__PURE__ */ new Map();
-  let s, d = !0;
-  function c(n) {
-    return ["control", "shift", "alt", "meta"].includes(n.toLowerCase());
-  }
-  function h(n) {
-    if (d) {
-      if (n.type == "keyup" && !c(n.key)) {
-        s = n.key.toLowerCase();
-      } else if (n.type == "keydown") {
-        const o = {
-          ctrl: n.ctrlKey,
-          shift: n.shiftKey,
-          alt: n.altKey,
-          meta: n.metaKey,
-          key: n.key.toLowerCase(),
-        };
-        for (const [f, a] of t.entries()) {
-          const e = a, i = e.bindings[e.level];
-          i.ctrl == o.ctrl && i.shift == o.shift && i.alt == o.alt &&
-            o.key == i.key
-            ? e.bindings.length == 1
-              ? f()
-              : (e.level == 0 || e.bindings[e.level - 1].key == s) &&
-                (e.level++,
-                  e.onComboChange instanceof Function &&
-                  e.onComboChange(e.level),
-                  e.level == e.bindings.length && (f(), e.level = 0))
-            : (e.onComboChange instanceof Function && e.onComboChange(0),
-              e.level = 0);
+  function t(e) {
+    if (l) {
+      if (e.type == "keyup" && !s(e.key))
+        a = e.key.toLowerCase();
+      else if (e.type == "keydown") {
+        const { ctrlKey: y, shiftKey: d, altKey: p, metaKey: f } = e, o = e.key.toLowerCase();
+        for (const [i, u] of r.entries()) {
+          const { onComboChange: m, combo: h, bindings: b } = u, w = b[h];
+          w.ctrlKey == y && w.shiftKey == d && w.altKey == p && w.metaKey == f && w.key == o && (h == 0 || b[h - 1].key == a || b[h - 1].key == c) ? (u.combo++, m instanceof Function && m(u.combo), u.combo == b.length && (i(), u.combo = 0)) : (m(h), u.combo = 0);
         }
+        s(e.key) && (c = e.key);
       }
     }
   }
-  l.addEventListener("keydown", h), l.addEventListener("keyup", h);
-  let r = [];
-  const y = (n) => r.push(n);
+  n.addEventListener("keydown", t), n.addEventListener("keyup", t);
+  let k = [];
+  const K = (e) => k.push(e);
   return {
     /**
      * Listen on certain key sequence.
@@ -111,60 +75,69 @@ function u(l) {
      * @param {Function} onComboChange - The progress callback function.
      *
      * @example
-     * on("C-s a", () => {
+     * kikey.on("C-s a", () => {
      *   console.log("Press Ctrl+s and release, then press a.");
      * })
      */
-    on(n, o, f = null) {
-      const a = n.split(" ").filter((e) => e !== "");
-      t.set(o, {
-        bindings: a.map(k),
-        level: 0,
-        callback: o,
-        onComboChange: f,
+    on(e, y = () => {
+    }, d = () => {
+    }) {
+      const p = e.split(" ").filter((f) => f !== "").map(
+        L
+      );
+      r.set(y, {
+        bindings: p,
+        combo: 0,
+        onComboChange: d
       });
     },
     /**
      * Remove the callback.
      */
-    off(n) {
-      t.delete(n);
+    off(e) {
+      r.delete(e);
     },
     /**
-     * Enable kikey
+     * Enable kikey.
      */
     enable() {
-      d = !0;
+      l = !0;
     },
     /**
-     * Disable kikey
+     * Disable kikey.
      */
     disable() {
-      d = !1;
+      l = !1;
     },
+    /**
+     * Start recording shortcut.
+     */
     startRecord() {
-      l.addEventListener("keydown", y), l.addEventListener("keyup", y);
+      n.addEventListener("keydown", K), n.addEventListener("keyup", K);
     },
+    /**
+     * Stop recording shortcut.
+     */
     stopRecord() {
-      l.removeEventListener("keydown", y), l.removeEventListener("keyup", y);
-      let n = [];
-      for (let o = 0; o < r.length; o++) {
-        const f = r[o], a = o == r.length - 1 ? null : r[o + 1];
-        if (f.type == "keydown" && (a === null || a.type == "keyup")) {
-          let e = "";
-          e += f.ctrlKey ? "C" : "",
-            e += f.shiftKey ? "S" : "",
-            e += f.altKey ? "A" : "",
-            e += f.metaKey ? "M" : "",
-            e = e.split("").join("-");
+      var p;
+      n.removeEventListener("keydown", K), n.removeEventListener("keyup", K);
+      const e = [];
+      let y = 0, d = 1;
+      for (; y < k.length; ) {
+        const f = k[y];
+        if (f.type == "keydown" && ((p = k.at(d)) == null ? void 0 : p.type) == "keyup") {
+          let o = f.ctrlKey ? "C" : "";
+          o += f.shiftKey ? "S" : "", o += f.altKey ? "A" : "", o += f.metaKey ? "M" : "", o = o.split("").join("-");
           let i = f.key.toLowerCase();
-          i == " " ? i = "space" : i == "-" ? i = "dash" : c(i) && (i = null),
-            e.length > 0 && i ? e += "-" + i : e.length == 0 && (e = i),
-            n.push(e);
+          i == " " ? i = "space" : i == "-" ? i = "dash" : s(i) && (i = null), o.length > 0 && i ? o += "-" + i : o.length == 0 && (o = i), e.push(o);
         }
+        y++, d++;
       }
-      return r = [], n.join(" ");
-    },
+      return k = [], e.join(" ");
+    }
   };
 }
-export { k as makeBinding, u as default };
+export {
+  C as default,
+  L as makeBinding
+};
